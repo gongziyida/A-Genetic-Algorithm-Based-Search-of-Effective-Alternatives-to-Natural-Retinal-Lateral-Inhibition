4ed811610fb8fecd2bc25a4883b60befab1797a6
Ziyi Gong <zig9@pitt.edu>
2019-10-30 18:10:28 -0400

    -

---
+++ perceptron.c
@@ -0,0 +1,5 @@
+//
+// Created by ziyi on 10/30/19.
+//
+
+#include "perceptron.h"
---
+++ perceptron.h
@@ -0,0 +1,8 @@
+//
+// Created by ziyi on 10/30/19.
+//
+
+#ifndef MY_PROJECT_PERCEPTRON_H
+#define MY_PROJECT_PERCEPTRON_H
+
+#endif //MY_PROJECT_PERCEPTRON_H
