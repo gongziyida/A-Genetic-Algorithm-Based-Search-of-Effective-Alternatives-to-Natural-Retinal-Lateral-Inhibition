b977d4b2e502d35333b4ba2178b56b1a8a161051
Ziyi Gong <zig9@pitt.edu>
2019-11-24 19:54:52 -0500

    separate visualization

--- visualization.py
+++ visualization.py
@@ -6,9 +6,9 @@ NO_NON_TRIVIAL_W = 'gray'
 
 def save_img(fname):
     i = 2
-    g = nx.MultiDiGraph()
     counter = 0
     n_cells = {}
+    connections = {}
 
     with open(fname + '.txt', 'r') as f:
         lines = f.readlines()
@@ -26,12 +26,10 @@ def save_img(fname):
 
                 txt = [l.split() for l in lines[i+1 : i+1+m]]
 
-                connection = np.array(txt, dtype=np.float64).reshape((m, n))
+                c = np.array(txt, dtype=np.float64).reshape((m, n))
+                if c.any(): # any non-zeros
+                    connections.update({(s, t): c})
 
-                for p in range(m):
-                    for q in range(n):
-                        if np.abs(connection[p, q]) > 0.1:
-                            g.add_edge((s, q), (t, p), color=connection[p, q])
 
                 i += m + 1
             else:
@@ -41,31 +39,46 @@ def save_img(fname):
         print('No interneuron for', fname)
         return float(lines[0]), NO_INTERNEURON
 
-    # Extract a list of edge colors
-    edges = g.edges()
-    if len(edges) == 0:
+    if len(connections) == 0:
         print('No non-trivial weight for', fname)
         return float(lines[0]), NO_NON_TRIVIAL_W
 
-    edge_colors = [g[u][v][0]['color'] for u, v in edges]
+    fig, ax = plt.subplots(len(connections))
+    fig.set_size_inches(5, 5 * len(connections))
 
-    # define fixed positions of nodes
-    nodes = g.nodes()
-    fixed_pos = {}
-    for n in nodes:
-        fixed_pos.update({n: (n[0], (n[1] - n_cells[n[0]]/2) * 2)})
+    for i, (s, t) in enumerate(connections.keys()):
+        g = nx.MultiDiGraph()
 
-    # get the nx positions
-    pos = nx.spring_layout(g, pos=fixed_pos, fixed=fixed_pos.keys())
+        n = n_cells[s]
+        m = n_cells[t]
+        c = connections[(s, t)]
 
-    # draw
-    fig, ax = plt.subplots(1)
-    fig.set_size_inches(8, 8)
-    nx.draw(g, pos=pos, node_size=100, node_color='black',
-            edge_cmap=plt.get_cmap('bwr'), edge_color=edge_colors, edge_vmin=-1, edge_vmax=1,
-            connectionstyle='arc3,rad=0.1', ax=ax)
-    ax.text(0.4, 0.9, 'cost = ' + lines[0], transform=ax.transAxes, fontsize=11)
-    fig.savefig(fname, dpi=300, bbox_inches='tight')
+        for p in range(m):
+            for q in range(n):
+                if np.abs(c[p, q]) > 0.01:
+                    g.add_edge((s, q), (t, p), color=c[p, q])
+
+        # Extract a list of edge colors
+        edges = g.edges()
+        edge_colors = [g[u][v][0]['color'] for u, v in edges]
+
+        # define fixed positions of nodes
+        nodes = g.nodes()
+        fixed_pos = {}
+        for n in nodes:
+            fixed_pos.update({n: (n[0], (n[1] - n_cells[n[0]]/2) * 2)})
+
+        # get the nx positions
+        pos = nx.spring_layout(g, pos=fixed_pos, fixed=fixed_pos.keys())
+
+        # draw
+        nx.draw(g, pos=pos, node_size=50, node_color='black',
+                edge_cmap=plt.get_cmap('bwr'), edge_color=edge_colors, edge_vmin=-1, edge_vmax=1,
+                ax=ax[i])
+
+        ax[i].text(0.5, 1, '%d -> %d' % (s, t), transform=ax[i].transAxes, fontsize=10)
+
+    fig.savefig(fname, dpi=200, bbox_inches='tight')
 
     return float(lines[0]), COMMON
 
